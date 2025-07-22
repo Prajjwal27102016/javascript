503c68afc28afc39a1ca4c07f28cfde78b85d682
Prajjwal27102016 <mandloidilip1@gmail.com>
2025-07-22 18:28:22 +0530

    done

--- for fun.js and  .py/js.js
+++ for fun.js and  .py/js.js
@@ -1,11 +1,11 @@
-var myForm = document.getElementById("prajjwal");
+var myForm = document.getElementById("kamla");
 var myInput = document.getElementById("myInput");
 var myItem = document.getElementById("myItem");
-myForm.addEventListener("submit", function(event) {
+myForm.addEventListener("submit", function (event) {
     event.preventDefault();
     createItem(myInput.value);
 });
-function createItem(inputItems){
+function createItem(inputItems) {
     var items = `<li>${inputItems}<button onclick = "deleteElement(this)">delete product</button> </li>`;
     myItem.insertAdjacentHTML("beforeend", items);
     myInput.value = "";
