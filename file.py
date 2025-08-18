5dae03540ffda44387ddbdbc44101f4dc58cdf18
Prajjwal27102016 <mandloidilip1@gmail.com>
2025-08-18 16:27:04 +0530

    done

--- file.py
+++ file.py
@@ -3,3 +3,12 @@ print("10th")
 print("19th")
 print("28th")
 print("27th")
+
+var1 = "Hello"
+print(type(var1))
+var2 = 5
+print(type(var2))
+var3 = 2.5
+print(type(var3))
+var4 = True
+print(type(var4))
