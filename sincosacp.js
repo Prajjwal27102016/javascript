025ffdfc41bf9799662d9b136cb40eb726359a1e
Prajjwal27102016 <mandloidilip1@gmail.com>
2026-02-19 18:51:16 +0530

    done

---
+++ sincosacp.js
@@ -0,0 +1,7 @@
+var angle = 45 * Math.PI / 180; 
+var value = Math.sin(angle);
+console.log("Sin 45° is : " + value);
+var value1 = Math.cos(angle);
+console.log("Cos 45° is : " + value1);
+var value2 = Math.tan(angle);
+console.log("Tan 45° is : " + value2);
