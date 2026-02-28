c26a7219ea698c3175c87c2b9d25a8ac6e6634a1
Prajjwal27102016 <mandloidilip1@gmail.com>
2026-02-28 16:24:27 +0530

    done

---
+++ testing.js
@@ -0,0 +1,30 @@
+function testing() {
+
+    if (true) {
+
+        var x = 10;
+    }
+
+    console.log(x);
+
+}
+
+testing()
+
+function testing() {
+
+    if (true) {
+
+        var y = 20;
+    }
+
+    console.log(y);
+
+}
+testing()
+
+var x = 10;
+var y = 20;
+var sum = x + y;
+
+console.log(sum);
