32a06c895fd5043a9b929ad4d7525284c4d7b916
Prajjwal27102016 <mandloidilip1@gmail.com>
2026-03-21 16:58:21 +0530

    done

--- task.js
+++ task.js
@@ -1,10 +1,1 @@
-function divisible_5(num) {
-    if (num % 5 === 0) {
-        return num + ' is divisible with 5';
-    } else {
-        return num + ' is not divisible with 5';
-    }
-}
-
-console.log(divisible_5(10));
-console.log(divisible_5(12)); 
+True=100
